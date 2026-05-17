5df0690ed822365c912dd40b5db5c731d29e1bb3
Anna Huang <76632032+sayoku@users.noreply.github.com>
2026-05-17 18:51:21 -0400

    fixing spelling mistake in subgraph query

--- src/kgraph/query.py
+++ src/kgraph/query.py
@@ -235,7 +235,7 @@ class KGQueryAgent():
             return self.find_by_type(parameters.get("node_type", ""))
         elif operation == "find_by_name":
             return self.find_by_name(parameters.get("name_pattern", ""))
-        elif operation == "find_neightbors":
+        elif operation == "find_neighbors":
             return self.find_neighbors(parameters.get("node_name", ""), parameters.get("max_depth", 1))
         elif operation == "find_by_topic":
             return self.find_by_topic(parameters.get("topic", ""))
